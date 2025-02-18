c7f97439beabf5c55fd67afced12e66b4b617e8a
Audace SANGANO <audasang2018@gmail.com>
2025-02-18 12:45:48 +0200

    set winner

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,29 @@ export default function Board() {
   const [square, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
   function handleClick(i) {
+    if (square[i] || calculateWinner(square)) {
+      return;
+    }
     const nextSquares = square.slice();
     if (xIsNext) {
       nextSquares[i] = "X";
-    }else{
+    } else {
       nextSquares[i] = "O";
     }
     setSquares(nextSquares);
-    setXIsNext(!xIsNext)
+    setXIsNext(!xIsNext);
+  }
+  const winner = calculateWinner(square);
+  let statusResult;
+
+  if (winner) {
+    statusResult = "Winner: " + winner;
+  } else {
+    statusResult = "Next player: " + (xIsNext ? "X" : "O");
   }
   return (
     <>
+      <div className="status">{statusResult}</div>
       <div className="board-row">
         <Square square={square[0]} onSquareClick={() => handleClick(0)} />
         <Square square={square[1]} onSquareClick={() => handleClick(1)} />
@@ -33,4 +45,24 @@ export default function Board() {
       </div>
     </>
   );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
