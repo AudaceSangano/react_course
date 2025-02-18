4b905fc596e97d83bb2b2be55f7a357bedb82fe5
Audace SANGANO <audasang2018@gmail.com>
2025-02-18 12:36:08 +0200

    set Turn

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,35 @@
+import { useState } from "react";
 import Square from "./components/Square";
 
 export default function Board() {
+  const [square, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+  function handleClick(i) {
+    const nextSquares = square.slice();
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    }else{
+      nextSquares[i] = "O";
+    }
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext)
+  }
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square square={square[0]} onSquareClick={() => handleClick(0)} />
+        <Square square={square[1]} onSquareClick={() => handleClick(1)} />
+        <Square square={square[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square square={square[3]} onSquareClick={() => handleClick(3)} />
+        <Square square={square[4]} onSquareClick={() => handleClick(4)} />
+        <Square square={square[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square square={square[6]} onSquareClick={() => handleClick(6)} />
+        <Square square={square[7]} onSquareClick={() => handleClick(7)} />
+        <Square square={square[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
   );
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,9 +1,4 @@
-import { useState } from "react";
-
-export default function Square() {
-    const [value, setValue] = useState(null)
-    function handleClick() {
-        setValue('X');
-    }
-    return <button className="square" onClick={handleClick}>{value}</button>;
+// eslint-disable-next-line react/prop-types
+export default function Square({square, onSquareClick}) {
+    return <button className="square" onClick={onSquareClick}>{square}</button>;
 }
